83ec6af5670b20fc2cf2769bc99f967bfac2123e
Khanhdeux Mac <khanhdeux@gmail.com>
2017-08-30 19:37:45 +0200

    Finish style recipes

--- wp-content/plugins/bok/templates/template-recipes.php
+++ wp-content/plugins/bok/templates/template-recipes.php
@@ -13,6 +13,26 @@
         <div class="row text-center">
             <h2>Rezepte</h2>
         </div>
+        <?php
+        while ( have_posts() ) : the_post(); ?> <!--Because the_content() works only inside a WP Loop -->
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <?php the_content() ?>
+                         </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div style='border-bottom:1px solid #ccc;margin:10px auto 30px auto;'></div>
+                </div>
+            </div>
+        <?php
+        endwhile; //resetting the page loop
+        wp_reset_query(); //resetting the page query
+        ?>
         <div class="row">
             <?php $loop = new WP_Query( array( 'post_type' => 'recipe', 'posts_per_page' => 10 ) ); ?>
 
