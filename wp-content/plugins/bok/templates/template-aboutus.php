bb7c021daf15a771a3adc370e9cc50d1fd212cd0
Khanhdeux Mac <khanhdeux@gmail.com>
2017-11-13 23:49:41 +0100

    Create about-us page

--- wp-content/plugins/bok/templates/template-aboutus.php
+++ wp-content/plugins/bok/templates/template-aboutus.php
@@ -7,14 +7,18 @@
  * Theme Name: Rydon 2.0
  */
 get_header('bok'); ?>
-<div id="main-bok-content">
+<div id="main-bok-content" class="about-us">
+    <div class="row bok-restaurant-top">
+        <img src="<?php echo esc_url( plugins_url( BOK__PLUGIN_NAME . '/images/restaurant.png') ); ?>" width="100%" />
+        <div class="welcome-text">Über uns</div>
+    </div>
+    <div class="row about-us-description"">
+        Lorem Ipsum ist ein einfacher Demo-Text für die Print- und Schriftindustrie. Lorem Ipsum ist in der Industrie bereits der Standard Demo-Text seit 1500, als ein unbekannter Schriftsteller eine Hand voll Wörter nahm und diese durcheinander warf um ein Musterbuch zu erstellen. Es hat nicht nur 5 Jahrhunderte überlebt, sondern auch in Spruch in die elektronische Schriftbearbeitung geschafft (bemerke, nahezu unverändert). Bekannt wurde es 1960, mit dem erscheinen von "Letraset", welches Passagen von Lorem Ipsum enhielt, so wie Desktop Software wie "Aldus PageMaker" - ebenfalls mit Lorem Ipsum.
+    </div>
     <?php while ( have_posts() ) : the_post(); ?>
         <div class="section single-post">
             <section>
                 <div class="container container-800">
-                    <div class="row text-center">
-                        <h2>Über uns</h2>
-                    </div>
                     <?php $loop = new WP_Query( array( 'post_type' => 'story', 'posts_per_page' => 10 ) ); $count = 0; ?>
 
                     <?php while ( $loop->have_posts() ) : $loop->the_post();
@@ -49,7 +53,6 @@ get_header('bok'); ?>
                 </div>
             </section>
         </div>
-
     <?php endwhile; ?>
 </div>
 
