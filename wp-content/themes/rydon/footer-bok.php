ab149592b3d2ea22bf69e253b95d7e0aab477139
Khanhdeux Mac <khanhdeux@gmail.com>
2018-05-04 21:42:14 +0200

    footer

--- wp-content/themes/rydon/footer-bok.php
+++ wp-content/themes/rydon/footer-bok.php
@@ -11,37 +11,29 @@
 <div id="bok-footer">
     <div class="container container-800">
         <div class="row">
-            <div class="col-sm-2 col-xs-6">
-                <h4><a href="<?php echo get_site_url() . '/aboutus'; ?>">Über BOK</a></h4>
+            <div class="col-sm-3 col-xs-6">
+                <h4><a href="<?php echo get_site_url() . '/restaurants'; ?>">Restaurants</a></h4> <br />
+                <p><a href="<?php echo get_site_url() . '/restaurant/schulterblatt/'; ?>">Schulterblatt</a></p>
+                <p><a href="<?php echo get_site_url() . '/restaurant/ottensen/'; ?>">Ottensen</a></p>
+                <p><a href="<?php echo get_site_url() . '/restaurant/europapassage/'; ?>">Europa Passage</a></p>
             </div>
-            <div class="col-sm-2 col-xs-6">
-                <h4><a href="<?php echo get_site_url() . '/restaurants'; ?>">Restaurants</a></h4>
-            </div>
-            <div class="col-sm-2 col-xs-6">
-                <h4><a href="<?php echo get_site_url() . '/news'; ?>">Neuigkeiten</a></h4>
+            <div class="col-sm-3 col-xs-6">
+                <h4><a href="<?php echo get_site_url() . '/news'; ?>">Neuigkeiten</a></h4> <br />
+                <?php $loop = new WP_Query( array( 'post_type' => 'news', 'posts_per_page' => 10, 'order' => 'DESC' ) ); ?>
+                <?php while ( $loop->have_posts() ) : $loop->the_post();?>
+                    <p><a href="<?php the_permalink(); ?>"><b><?php the_title() ?></b></a></p>
+                    <p><?php $content = get_the_content(); echo mb_strimwidth($content, 0, 100, '...');?></p>
+                <?php endwhile; ?>
+
             </div>
             <div class="col-sm-2 col-xs-6">
                 <h4><a href="<?php echo get_site_url() . '/recipes'; ?>">Rezepte</a></h4>
             </div>
             <div class="col-sm-2 col-xs-6">
-                <h4><a href="<?php echo get_site_url() . '/contact'; ?>">Kontakt</a></h4>
+                <h4><a href="<?php echo get_site_url() . '/aboutus'; ?>">Über Uns</a></h4>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-1 col-xs-12">
-                <img src="<?php echo esc_url( plugins_url( BOK__PLUGIN_NAME . '/images/flag.jpg') ); ?>" width="15" />
-            </div>
-            <div class="col-sm-5 col-xs-12">
-                2017 Bok, Inc.  All Rights Reserved
-            </div>
-            <div class="col-sm-2 col-xs-12">
-                Guides
-            </div>
-            <div class="col-sm-2 col-xs-12">
-                Tearms of Use
-            </div>
-            <div class="col-sm-2 col-xs-12">
-                Bok privacy policy
+            <div class="col-sm-2 col-xs-6">
+                <h4><a href="http://www.boklieferservice.de/">Lieferservice</a></h4>
             </div>
         </div>
     </div>
