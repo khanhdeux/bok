3d6f0afc0d68fddcdea934ffa90eb1dfba18f6ad
Khanhdeux Mac <khanhdeux@gmail.com>
2018-05-24 23:35:45 +0200

    news templates

--- wp-content/plugins/bok/templates/template-news.php
+++ wp-content/plugins/bok/templates/template-news.php
@@ -19,38 +19,28 @@ get_header('bok'); ?>
             <p>Follow us on the latest news, promotions and a few surprises along the way</p>
         </div>
     </div>
-    <div class="container container-800">
-        <div class="row text-center">
-            <h2>Neuigkeiten</h2>
-        </div>
-
+    <div class="container" style="width: 100%">
         <?php $loop = new WP_Query( array( 'post_type' => 'news', 'posts_per_page' => 10 ) ); $count = 0; ?>
 
-        <?php while ( $loop->have_posts() ) : $loop->the_post();
-            $count++;
-         ?>
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                            <div class="news">
-                                <div class="thumbnail-<?php echo ((($count % 2) == 0 ) ? "right" : "left"); ?> media-top">
-                                    <a class="thumbnail" href="<?php the_permalink(); ?>">
-                                        <?php if ( has_post_thumbnail() ) : ?>
-                                            <img class="media-object" src="<?php the_post_thumbnail_url('full'); ?>" alt="<?php the_title_attribute(); ?>">
-                                        <?php endif; ?>
-                                    </a>
-                                </div>
-                                <div class="text-justify">
-                                    <h4 class="media-heading"><?php the_title() ?></h4>
-                                    <?php the_content() ?>
-                                </div>
-                            </div>
+        <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+            <?php if ($count %2 == 0) : ?>
+                <div class="row bok-news-row">
+            <?php endif; ?>
+                    <div class="col-sm-6 bok-news-col">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php if ( has_post_thumbnail() ) : ?>
+                                <img src="<?php the_post_thumbnail_url('full'); ?>" alt="<?php the_title_attribute(); ?>" />
+                            <?php endif; ?>
+                        </a>
+                        <div class="text-center">
+                            <h4><?php the_title() ?></h4>
+                            <div><a class="link" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">Mehr</a></div>
                         </div>
                     </div>
+            <?php if ($count %2 != 0) : ?>
                 </div>
-            </div>
-
+            <?php endif; ?>
+        <?php $count++; ?>
         <?php endwhile; ?>
     </div>
 </div>
