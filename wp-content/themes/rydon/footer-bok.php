25dfa16d65c06a4b83a65de632dc8fbe4f58b21a
Khanhdeux Mac <khanhdeux@gmail.com>
2018-06-01 18:59:49 +0200

    Bug footer

--- wp-content/themes/rydon/footer-bok.php
+++ wp-content/themes/rydon/footer-bok.php
@@ -9,19 +9,19 @@
 ?>
 
 <div id="bok-footer">
-    <div class="container container-800">
+    <div class="container">
         <div class="row">
-            <div class="col-sm-3 col-xs-6">
+            <div class="col-sm-2 col-xs-6">
                 <h4><a href="<?php echo get_site_url() . '/restaurants'; ?>">Restaurants</a></h4> <br />
                 <p><a href="<?php echo get_site_url() . '/restaurant/schulterblatt/'; ?>">Schulterblatt</a></p>
                 <p><a href="<?php echo get_site_url() . '/restaurant/ottensen/'; ?>">Ottensen</a></p>
                 <p><a href="<?php echo get_site_url() . '/restaurant/europapassage/'; ?>">Europa Passage</a></p>
             </div>
-            <div class="col-sm-3 col-xs-6">
+            <div class="col-sm-4 col-xs-6">
                 <h4><a href="<?php echo get_site_url() . '/news'; ?>">Neuigkeiten</a></h4> <br />
                 <?php $loop = new WP_Query( array( 'post_type' => 'news', 'posts_per_page' => 10, 'order' => 'DESC' ) ); ?>
                 <?php while ( $loop->have_posts() ) : $loop->the_post();?>
-                    <p><a href="<?php the_permalink(); ?>"><b><?php the_title() ?></b></a></p>
+                    <p><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></p>
                 <?php endwhile; ?>
 
             </div>
